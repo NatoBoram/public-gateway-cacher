999637ef4d05f3d1064551d7670e3bde45651ded
Nathan Gendron <nathan.gendron@gcmi.ca>
2019-07-19 12:23:11 -0400

    --cid-version=1

--- publish-to-ipfs.sh
+++ publish-to-ipfs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ipfs add --recursive --quieter --wrap-with-directory --chunker=rabin index.html css images javascript json > public-gateway-cacher.ipfs
+ipfs add --recursive --quieter --wrap-with-directory --chunker=rabin --cid-version=1 index.html css images javascript json > public-gateway-cacher.ipfs
